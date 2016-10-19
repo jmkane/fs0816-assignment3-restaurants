20e3a635e0135cf13fd169824b950f5a24af330e
jmkane <jkane76@outlook.com>
2016-10-19 17:00:23 -0600

    Add files via upload

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
 import Express from 'express';
-
+import bodyParser from 'body-parser';
+import RestaurantRoutes  from './routes/restaurants';
 const app = Express();
 
-
 export default app;
+
+
+app.use(bodyParser.json());
+app.use('/restaurants', RestaurantRoutes);
+
+
+app.listen(3000, function(){
+  console.log('App has started');
+});
