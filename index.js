a2e8c00cc7016009758b8892dc98134809a7db42
jmkane <jkane76@outlook.com>
2016-10-19 16:59:50 -0600

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
-require('babel-register');
-module.exports = require('./src');
+import Express from 'express';
+import bodyParser from 'body-parser';
+import RestaurantRoutes  from './routes/restaurants';
+const app = Express();
 
+export default app;
+
+
+app.use(bodyParser.json());
+app.use('/restaurants', RestaurantRoutes);
+
+
+app.listen(3000, function(){
+  console.log('App has started');
+});
